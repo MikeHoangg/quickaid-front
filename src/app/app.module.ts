05de81b63cdd28bba0d5255169420792bc27088e
Mike Hoang <hoang.viet.ha@raccoongang.com>
2019-05-21 20:07:41 +0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { NotificationComponent } from './notification/notification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    EditProfileComponent,
+    ScheduleComponent,
+    NotificationComponent
   ],
   imports: [
     BrowserModule
