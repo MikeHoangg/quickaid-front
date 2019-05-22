0a2ef297a35bc04063c97b339fdf39eaf0453dc2
Mike Hoang <hoang.viet.ha@raccoongang.com>
2019-05-22 10:35:06 +0300

    Changed authorization components. Added redirects on successful authorization

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from '../api.service';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  error: any;
+  loginForm = new FormGroup({
+    email: new FormControl(null, [Validators.required]),
+    password: new FormControl(null, [Validators.required]),
+  });
 
-  constructor() { }
+  constructor(private api: ApiService,
+              private router: Router) {
+  }
 
   ngOnInit() {
   }
 
+  getError(field) {
+    if (this.loginForm.controls[field].hasError('required')) {
+      return 'required';
+    }
+  }
+
+  login() {
+    this.api.authorize('login', this.loginForm.value).subscribe((response: any) => {
+      if (response) {
+        this.error = null;
+        document.cookie = `auth_token=Token ${response.key};path=/`;
+        this.router.navigate(['/profile']);
+      } else {
+        this.error = this.api.errorLog.pop();
+      }
+    });
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from '../api.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm = new FormGroup({
+    email: new FormControl(null, [Validators.required]),
+    password1: new FormControl(null, [Validators.required]),
+    password2: new FormControl(null, [Validators.required]),
+  });
+  error: any;
 
-  constructor() { }
+  constructor(private router: Router,
+              private api: ApiService) {
+  }
 
   ngOnInit() {
   }
 
+  getError(field) {
+    if (this.registerForm.controls[field].hasError('required')) {
+      return 'required';
+    }
+  }
+
+  register() {
+    this.api.authorize('register', this.registerForm.value).subscribe((response: any) => {
+      if (response) {
+        document.cookie = `auth_token=Token ${response.key};path=/`;
+        this.error = null;
+        this.router.navigate(['/profile']);
+      } else {
+        this.error = this.api.errorLog.pop();
+      }
+    });
+  }
 }
